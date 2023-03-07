71cd385cc6c5ab3e9ffe1179590a7362b74c05bc
Papagem <126454492+Papagem@users.noreply.github.com>
2023-03-07 14:40:13 +0800

    Update myprojectapp.py

--- myprojectapp.py
+++ myprojectapp.py
@@ -1,15 +1,3 @@
-from sklearn import preprocessing
-scaler = preprocessing.MinMaxScaler()
-
-df = pd.read_csv('Advertising.csv')
-
-scaled=scaler.fit_transform(df)
-df_scaled = pd.DataFrame(scaled)
-df_scaled.head()
-
-df_scaled.to_csv('Advertising.csv',index=False)
-
-
 import streamlit as st
 import numpy as np
 import pandas as pd
