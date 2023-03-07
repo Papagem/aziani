a8e6ab627d5a056fe364c11767fc306b95de1c4a
Papagem <126454492+Papagem@users.noreply.github.com>
2023-03-07 14:58:46 +0800

    Update myprojectapp.py

--- myprojectapp.py
+++ myprojectapp.py
@@ -8,12 +8,52 @@ st.write(pd.DataFrame({
     'Churn Status': [0, 0, 0, 1]
 }))
 
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors = 5)
-knn
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
 
-knn.fit(Xtrain, ytrain)
+st.write("""
+# Simple Iris Flower Prediction App
 
-knn.score(Xtest, ytest)
+This app predicts the **Iris flower** type!
+""")
 
-from sklearn.metrics import accuracy_score
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
+    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
+    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+    Sales = st.sidebar.slider('Sales', 0.1, 2.5, 0.2)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper,
+            'Sales': Sales}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
