35be88785b5b5ad3e15be74db2fa9dd3ceb71c73
Papagem <126454492+Papagem@users.noreply.github.com>
2023-03-07 14:44:33 +0800

    Update myprojectapp.py

--- myprojectapp.py
+++ myprojectapp.py
@@ -7,3 +7,14 @@ st.write(pd.DataFrame({
     'Intplan': ['TV', 'Radio', 'Newspaper', 'Sales'],
     'Churn Status': [0, 0, 0, 1]
 }))
+
+from sklearn.naive_bayes import GaussianNB # 1. choose model class
+model = GaussianNB()                       # 2. instantiate model
+model.fit(Xtrain, ytrain)                  # 3. fit model to data
+y_model = model.predict(Xtest)             # 4. predict on new data
+
+from sklearn.metrics import accuracy_score
+accuracy_score(ytest, y_model)
+from sklearn.metrics import classification_report
+
+print(classification_report(ytest, y_model)) 
