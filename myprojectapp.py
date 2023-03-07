024cccfb014e0c3c5467ed75f74b70ede0864428
Papagem <126454492+Papagem@users.noreply.github.com>
2023-03-07 16:13:03 +0800

    Update myprojectapp.py

--- myprojectapp.py
+++ myprojectapp.py
@@ -15,7 +15,7 @@ def user_input_features():
     TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
     Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
     Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
-        data = {'TV': TV,
+             data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper,
             'Sales': Sales}
